82351017d8f6234adf26e38bc6cd3e001a17dfac
Daydream0929 <2352869723@qq.com>
2023-10-02 23:26:07 +0800

    feat(main):初始化命令行聊天室

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 冯振军 on 2023/10/2.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "--------这是一个多人聊天平台--------" << endl;
+
+    bool flag = false;
+    do {
+        cout << ": " << endl;
+    } while (flag);
+
+    return 0;
+}
